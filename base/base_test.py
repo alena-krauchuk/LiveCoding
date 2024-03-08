3fbb16c279dd54e7f9e4143896dc6d0ece774e67
Alena Krauch <peregovorun@gmail.com>
2024-03-09 00:25:44 +0300

    First CI push

---
+++ base/base_test.py
@@ -0,0 +1,46 @@
+import pytest
+from config.data import Data
+from pages.login_page import LoginPage
+from pages.dashboard_page import DashboardPage
+from pages.personal_page import PersonalPage
+
+
+class BaseTest:
+    data: Data
+
+    # аннотация типов
+    login_page: LoginPage
+    dashboard_page: DashboardPage
+    personal_page: PersonalPage
+
+    # фикстура
+    @pytest.fixture(autouse=True)
+    def setup(self, request, driver):
+        request.cls.driver = driver
+        request.cls.data = Data()
+        request.cls.login_page = LoginPage(driver)
+        request.cls.dashboard_page = DashboardPage(driver)
+        request.cls.personal_page = PersonalPage(driver)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
